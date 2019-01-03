cc18413cb3cd3426367a1c114b75420b3f3b0eb7
Rukiya Hassan <rukiya.hassan@gmail.com>
2019-01-03 14:01:58 -0700

    added important comments i think

--- meshToCoords.py
+++ meshToCoords.py
@@ -13,17 +13,17 @@ def parse(vector):
     sx = splitString[1]
     sy = splitString[2]
     sz = splitString[3]
-    x = int(float(sx)*100)
-    y = int(float(sy)*100)
-    z = int(float(sz)*100)
+    x = int(float(sx)*81.92)    # multiply by level's width!
+    y = int(float(sy)*81.92)    # multiply by level's width!
+    z = int(float(sz)*90)
     
     coords = [x, y]
     
     return coords
 
-
+# run script in object mode!
 print("writing...")
-f = open("/home/rukiya/eclipse-workspace/flyingGame/level0.txt", "w")
+f = open("/home/rukiya/eclipse-workspace/flyingGame/level1-ground.txt", "w")
 for item in bpy.data.objects:
     if item.type == 'MESH' and item.name == 'level':
         for face in item.data.polygons:
